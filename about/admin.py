d4a70e2b19de971c7f1eeb4ed1a175f19b59fa55
Cristiana David <cristiana.radu92@yahoo.com>
2025-07-08 21:07:31 +0100

    Arrange all files to pass validation

--- about/admin.py
+++ about/admin.py
@@ -4,20 +4,16 @@ from .models import About, CollaborateRequest
 
 
 @admin.register(About)
-# Register your models here.
 class AboutAdmin(SummernoteModelAdmin):
-
+    """
+    Adds rich-text editing of content in admin
+    """
     summernote_fields = ('content',)
-
-# Note: admin.ModelAdmin is the standard way of registering
-#       our model with the admin panel. We do it differently
-#       above because we are supplying Summernote fields.
-#       If you want to customise the admin panel view in your
-#       own projects, then inherit from admin.ModelAdmin like
-#       we do below.
 
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
+    """
+    Lists message and read fields for display in admin
+    """
     list_display = ('message', 'read',)
